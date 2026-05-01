e403b034db789935a8e7be6ecec0b50a5c5dbddd
shiro2019 <shuyi011@e.ntu.edu.sg>
2026-05-01 12:04:44 +0800

    remove temporary test files

--- test_api.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
-
-load_dotenv()
-
-llm = ChatOpenAI(model="gpt-4o-mini")
-response = llm.invoke("What is the Citi Rewards Card earn rate in Singapore?")
-print(response.content)
